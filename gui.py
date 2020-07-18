c53d1e27f42a042dd15b29ccf7749539cea53971
samlentz <66572076+samlentz@users.noreply.github.com>
2020-07-18 00:54:14 -0500

    Add files via upload

--- gui.py
+++ gui.py
@@ -5,6 +5,51 @@ import pets
 import requests
 import webbrowser
 import pickle
+import time
+
+xloc = 200
+yloc = 400
+desx = 200
+desy = 400
+framecount=1
+
+def dogani():
+    global framecount
+    if framecount > 10000:
+        framecount = 1
+    global xloc
+    global yloc
+    diffx = 1 + int(abs((xloc - desx)/15))
+    diffy = 1+ int(abs((yloc-desy)/15))
+    
+    if xloc == desx and desy == yloc:
+        window_surface.blit((dog),(xloc,yloc))
+    elif(desx >= xloc):
+        if (framecount%60 ==0 ):
+            pet.playWith()
+        if(diffx < 3 and (diffy <3)):
+            window_surface.blit(walksright[framecount%5],(xloc,yloc))
+        else:
+            window_surface.blit(runright[framecount%4],(xloc,yloc))
+
+        xloc +=diffx
+    else:
+        if(diffx<3 and (diffy <3)):
+            window_surface.blit(walksleft[framecount%5],(xloc,yloc))
+        else:
+            window_surface.blit(runleft[framecount%5],(xloc,yloc))
+
+        xloc -=diffx
+    if (desy > yloc):
+        yloc += diffy
+    elif(desy < yloc):
+        yloc -= diffy
+    #print((xloc,yloc,desx,desy))
+
+
+
+
+
 
 print('1. New Game : 2. Load Save')
 i = input()
@@ -22,17 +67,20 @@ f.refresh()
 miles = f.getMiles()
 pet.refresh(miles)
 pet.timePass()
-g = pet.getStatus()
-print(g)
-name=g[0]
-age=g[1]
-happy=g[2]
-fill=g[3]
-dollas = g[4]
+
 pygame.init()
-
-
-
+runleft = []
+for i in range(1,6):
+    runleft.append(pygame.image.load('shep/Shepherd_run_'+str(i)+'.png'))
+runright = []
+for i in runleft:
+    runright.append(pygame.transform.flip(i,True,False))
+walksleft =[]
+for i in range(1,7):
+    walksleft.append(pygame.image.load('shep/Shepherd_walk_'+str(i)+'.png'))
+walksright =[]
+for i in walksleft:
+    walksright.append(pygame.transform.flip(i,True,False))
 
 
 pygame.display.set_caption('CyclePet')
@@ -41,7 +89,7 @@ window_surface = pygame.display.set_mode((800, 600))
 background = pygame.Surface((800, 600))
 background.fill(pygame.Color('#000000'))
 coin = pygame.image.load('goldCoin1.png')
-dog = pygame.image.load('Shepherd_default.png')
+dog = pygame.image.load('shep/Shepherd_default.png')
 
 manager = pygame_gui.UIManager((800, 600))
 pygame.draw.rect(background,pygame.Color('#96f97b'),((0,0),(800,70)))
@@ -51,17 +99,18 @@ text1 = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((60, 35), (90, 30)
 text3 = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((475, 10), (100, 25)),text ='Miles banked',manager = manager)
 text6 = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((475, 30), (100, 25)),text ='Age (Hours)',manager = manager)
 
-background.blit(coin,(575,10))
-
-dirt = pygame.image.load('grass.png')
+background.blit(coin,(575,8))
+bgs = []
+for i in pet.background:
+    bgs.append(pygame.image.load(i))
 clock = pygame.time.Clock()
 is_running = True
 shopopen = True
 
-age = (pet.getStatus())[1]/3600
 
 
 while is_running:
+    framecount+=1
     time_delta = clock.tick(60)/1000.0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -76,10 +125,22 @@ while is_running:
         if event.type == pygame.USEREVENT:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == shop:
-                    print('Hello World!')
+                    pet.runStore()
+        if event.type == pygame.MOUSEBUTTONUP:
+            desx,desy = pygame.mouse.get_pos()
+
         manager.process_events(event)
 
         manager.update(time_delta)
+    g = pet.getStatus()
+    name=g[0]
+    age = (g[1]/3600)
+    happy=g[2]
+    fill=g[3]
+    dollas = g[4]
+    bglevel = g[5]
+    if(g[6]):
+        is_running = False
     pygame.draw.rect(background,pygame.Color('#929591'),((250,10),((150,20))))
     pygame.draw.rect(background,pygame.Color('#ffb07c'),((250,10),((150/10)*fill,20)))
     pygame.draw.rect(background,pygame.Color('#929591'),((30,10),((150,20))))
@@ -88,11 +149,12 @@ while is_running:
     text4 = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((610, 30), (60, 30)),text =str(int(age)),manager = manager)
 
     
-    
     window_surface.blit(background, (0, 0))
-    window_surface.blit(dirt,(0,70))
-    window_surface.blit(dog,(200,300))
+    window_surface.blit(bgs[bglevel],(0,70))
+    dogani()
+
     manager.draw_ui(window_surface)
 
     pygame.display.update()
-    clock.tick(25)
+    clock.tick(10)
+
--- pets.py
+++ pets.py
@@ -7,6 +7,11 @@ class pet:
     latestmiles = 0
     balance = 0
     lastupdated = 0
+    itemList = ['Food              ','Toy               ','Background Upgrade','Nothing           ',]
+    itemCosts = [2,1,20,0]
+    background = ['dirt.png','grass.png','floor.jpg','marble.jpg']
+    bgLevel = 0
+    dead = False
     def __init__(self,name,originalmiles):
         self.name = name
         self.birthtime = self.time.time()
@@ -14,23 +19,70 @@ class pet:
         self.happy = 3
         self.latestmiles=originalmiles
         self.lastupdated = self.time.time()
+    def playWith(self):
+        self.happy +=.5
+        if(self.happy >10):
+            self.happy = 10
     def getStatus(self):
-        return [self.name,self.time.time()-self.birthtime,self.happy,self.fill,self.balance]
+        return [self.name,self.time.time()-self.birthtime,self.happy,self.fill,self.balance,self.bgLevel,self.dead]
     def feed(self):
-        self.fill +=1
+        self.fill +=3
+        if self.fill > 10:
+            self.fill = 10
     def timePass(self):
         hrsDiff = (self.time.time()-self.lastupdated)/(60*60)
         self.fill -=.1*hrsDiff
         self.happy -= .1*hrsDiff
         self.lastupdated = self.time.time()
+        if(self.fill < 0 or self.happy < 0):
+            if not self.dead:
+                with open('graveyard.txt', 'a') as file:
+                    file.write("RIP to " + self.name + '. They died at only ' +str(int((self.time.time()-self.birthtime)/3600)) + ' hours old' + '\n')
+            self.dead = True
+            print("****Your pet has died. You can view him in the graveyard text file where you launch this game****")
     def buyToy(self):
-        self.happy +=.5
+        self.happy +=1.5
+        if(self.happy >10):
+            self.happy = 10
     def refresh(self,miles):
         if (miles == self.latestmiles):
             return
         else:
-            balance += miles-self.latestmiles
+            self.balance += miles-self.latestmiles
             self.latestmiles = miles
     def editMiles(self, num):
         self.balance+=nums
         return self.balance
+    def runStore(self):
+        self.printStore()
+        check =True
+        while(check):
+            print("What would you like to buy? Enter item number")
+            i = int(input())
+            if(self.itemCosts[i] <= self.balance):
+                self.balance -= self.itemCosts[i]
+                if(i==0):
+                    self.feed()
+                elif(i==1):
+                    self.buyToy()
+                elif(i==2):
+                    print(self.bgLevel,len(self.background))
+                    if(self.bgLevel < len(self.background)-1):
+                        self.bgLevel +=1
+                    else:
+                        print('background max level already achieved!')
+                        self.balance += self.itemCosts[i]
+                elif(i == 3):
+                    print('Leaving store')
+                elif(i ==4):
+                    self.fill = 0
+                check = False
+                print("Thank you for using the store")
+            else:
+                print('insufficiant funds')
+    def printStore(self):
+        print("-------STORE-------")
+        for i in range(len(self.itemList)):
+            print(str(i) + '. '+self.itemList[i] + '        ' + str(self.itemCosts[i]) +' Coins')
+        
+             
